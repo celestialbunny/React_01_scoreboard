1e897454664c15ca6c7bd9c650291a27bf474586
c0r0n4 <coronateh@outlook.com>
2019-01-28 10:57:22 +0800

    Updates

---
+++ precache-manifest.4b12374e2a71c3dce6d508adcedf1249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c4ac08451a3b656fc95",
+    "url": "/React_01_scoreboard/static/js/runtime~main.6c4ac084.js"
+  },
+  {
+    "revision": "6a920309fc497edf226d",
+    "url": "/React_01_scoreboard/static/js/main.6a920309.chunk.js"
+  },
+  {
+    "revision": "6be4241eb2f63ea29623",
+    "url": "/React_01_scoreboard/static/js/1.6be4241e.chunk.js"
+  },
+  {
+    "revision": "6a920309fc497edf226d",
+    "url": "/React_01_scoreboard/static/css/main.6fefe78d.chunk.css"
+  },
+  {
+    "revision": "6be4241eb2f63ea29623",
+    "url": "/React_01_scoreboard/static/css/1.7a171e21.chunk.css"
+  },
+  {
+    "revision": "bc0b1e23ca12b70a64e4c878bb0efe7d",
+    "url": "/React_01_scoreboard/index.html"
+  }
+];
